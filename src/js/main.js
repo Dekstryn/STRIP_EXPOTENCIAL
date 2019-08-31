60ee0e6a19eeb264da2104e1f25a49ce0f848f80
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-31 09:45:26 +0200

    Feat: local storage set

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
+const buttonStart = document.querySelector('.button_box__button--js')
+
+buttonStart.addEventListener('click', (e) =>{
+  const numberValue = document.getElementById('number').value;
+  localStorage.setItem("number", numberValue);
+  const exponentiationValue = document.getElementById('expo').value;
+  localStorage.setItem("exponentiation", exponentiationValue);
+})
 
 
 
