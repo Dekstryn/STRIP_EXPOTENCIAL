b07ddf85e30f19b0bfe6d158fb6ca10baf423922
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-08-31 10:19:40 +0200

    Feat: set div for display functions result

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 const buttonStart = document.querySelector('.button_box__button--js')
+const resultMultiplyText = document.querySelector('.result_multiply--js')
+const resultAddingText = document.querySelector('.result_adding--js')
 
 buttonStart.addEventListener('click', (e) =>{
   const numberValue = document.getElementById('number').value;
   localStorage.setItem("number", numberValue);
   const exponentiationValue = document.getElementById('expo').value;
   localStorage.setItem("exponentiation", exponentiationValue);
+  resultMultiplyText.textContent = "Liczba którą chcesz potęgować: " + numberValue;
+  resultAddingText.textContent = "Potęga, jaką ustawiłeś: " + exponentiationValue;
 })
 
 
