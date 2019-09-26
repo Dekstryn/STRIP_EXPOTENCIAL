8f5dcf17ebba04921e35218bacf8af88e563fa1b
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-09-26 06:01:31 +0200

    modified function parameters

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 const buttonStart = document.querySelector('.button_box__button--js')
 const resultMultiplyText = document.querySelector('.result_multiply--js')
 const resultAddingText = document.querySelector('.result_adding--js')
@@ -8,18 +9,22 @@ buttonStart.addEventListener('click', (e) =>{
   const exponentiationValue = document.getElementById('expo').value;
   localStorage.setItem("exponentiation", exponentiationValue);
   
-  function expotential (numberValue, exponentiationValue){
+  function expotential (number = localStorage.getItem("number"), exponentiation = localStorage.getItem("exponentiation")){
     //Variable definition
-    const numberStr = numberValue.toString();
+    const numberStr = number.toString();
     const i = 1;
     const a = numberStr;
     const b = numberStr;
+    const pot = exponentiation;
+    const lic = number;
+    
   }
 
   resultMultiplyText.textContent = "Liczba którą chcesz potęgować: " + numberValue;
   resultAddingText.textContent = "Potęga, jaką ustawiłeś: " + exponentiationValue;
 
 })
+
 
 
 
