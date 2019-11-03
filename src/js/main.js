9e7f704ee480a79cd7f2bbd595a3af98d55174ad
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2019-11-03 09:18:47 +0100

    Add validation to input number

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 "use strict"
+
 const buttonStart = document.querySelector('.button_box__button--js')
 const resultNumberText = document.querySelector('.result_number--js')
 const resultExponentiationText = document.querySelector('.result_exponentiation--js')
@@ -23,18 +24,106 @@ buttonStart.addEventListener('click', (e) =>{
   if (numberValue == 0){
     multiplyResult = 0;
     addingResult = 0;
+    
+    //print result
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
+    resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
+    resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
+    resultMultiply.textContent = `${multiplyResult}`;
+    resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
+    resultAdding.textContent = ` ${addingResult}`;
   }
   else if (exponentiationValue == 0){
     multiplyResult = 1;
     addingResult = 1;
+    
+    //print result
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
+    resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
+    resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
+    resultMultiply.textContent = `${multiplyResult}`;
+    resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
+    resultAdding.textContent = ` ${addingResult}`;
   }
   else if (exponentiationValue == 1){
     multiplyResult = numberValue;
     addingResult = numberValue;
+     
+    //print result
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
+    resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
+    resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
+    resultMultiply.textContent = `${multiplyResult}`;
+    resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
+    resultAdding.textContent = ` ${addingResult}`;
   }
   else if (numberValue == 1){
     multiplyResult = 1;
     addingResult = 1;
+    
+    //print result
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
+    resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
+    resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
+    resultMultiply.textContent = `${multiplyResult}`;
+    resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
+    resultAdding.textContent = ` ${addingResult}`;
+  }
+  else if (numberValue < 0){
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: "${numberValue}" jest mniejsza od zera.
+    Program obsługuje liczby naturalne z zakresu 0 - 10.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
+  }
+  else if (numberValue > 0 && numberValue < 1){
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: "${numberValue}" jest ułamkiem.
+    Program obsługuje liczby naturalne z zakresu 0 - 10.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
+  }
+
+  else if (numberValue > 10){
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: "${numberValue}" jest większa od 10.
+    Program obsługuje liczby naturalne z zakresu 0 - 10.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
+  }
+  else if (exponentiationValue < 0){
+    resultNumberText.textContent = `Potęga którą chcesz użyć: "${exponentiationValue}" jest mniejsza od zera.
+    Program obsługuje potęgi będące liczbami naturalnymi z zakresu 0 - 5.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
+  }
+  else if (exponentiationValue > 0 && exponentiationValue < 1){
+    resultNumberText.textContent = `Potęga którą chcesz użyć: "${exponentiationValue}" jest ułamkiem.
+    Program obsługuje potęgi będące liczbami naturalnymi z zakresu 0 - 5.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
+  }
+
+  else if (exponentiationValue > 5){
+    resultNumberText.textContent = `Potęga którą chcesz użyć: "${exponentiationValue}" jest większa od 5.
+    Program obsługuje potęgi będące liczbami naturalnymi z zakresu 0 - 5.`;
+    resultExponentiationText.textContent = ``;
+    resultMultiplyText.textContent = ``;
+    resultMultiply.textContent = ``;
+    resultAddingText.textContent = ``;
+    resultAdding.textContent = ``;
   }
   //calculations
   else {
@@ -61,14 +150,15 @@ buttonStart.addEventListener('click', (e) =>{
     else{
       addingResult = b;
     }
+    
+    //print result
+    resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
+    resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
+    resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
+    resultMultiply.textContent = `${multiplyResult}`;
+    resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
+    resultAdding.textContent = ` ${addingResult}`;
   }
-  //print result
-  resultNumberText.textContent = `Liczba którą chcesz potęgować: ${numberValue}`;
-  resultExponentiationText.textContent = `Potęga, jaką ustawiłeś: ${exponentiationValue}`;
-  resultMultiplyText.textContent = `Potęgowanie zapisane w formie mnożenia, będzie wyglądało następująco:`;
-  resultMultiply.textContent = `${multiplyResult}`;
-  resultAddingText.textContent = `Potęgowanie zapisane w formie dodawania, będzie wyglądało następująco:`;
-  resultAdding.textContent = ` ${addingResult}`;
 })
 
 
